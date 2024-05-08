284ea7772b5c2435351dfa63a94e72da252d5a0f
LValdes2003 <lvaldesp@myuax.com>
2024-05-08 11:37:13 +0200

    Ejercicio4

---
+++ src/Ejercicio4.java
@@ -0,0 +1,13 @@
+public class Ejercicio4 {
+    public int sumaDigitos(int n) {
+        // Si el número solo tiene un dígito, se retorna el mismo número
+        if(n < 10) {
+        return n;
+        } else {
+        // Se obtiene el último dígito y se suma con el resultado de la función
+            return n % 10 + sumaDigitos(n / 10);
+        }
+    }
+}
+// tipo de recursividad: recursividad de cola
+// complejidad de tiempo: O(log n)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ejercicio4 ej4 = new Ejercicio4();
+        System.out.println(ej4.sumaDigitos(140654));
     }
 }
